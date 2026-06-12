65038ff5fd045ae095f21d892cea462e87808cc3
marllonfilipe <mfcs1510@outlook.com>
2026-06-12 11:32:54 -0300

    Corrige host do Cloud Run no Vite

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      "dm2drefranqueado-398246262001.southamerica-east1.run.app",
+      ".run.app",
+    ],
+  },
 });
